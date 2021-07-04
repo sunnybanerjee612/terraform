e44da8490deea30eb715c7c506392348e2a01284
sunnybanerjee612 <83915611+sunnybanerjee612@users.noreply.github.com>
2021-07-03 20:52:43 -0500

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -12,7 +12,7 @@ terraform {
         region = "us-east-1"
     }
 
-    resource "aws_instance" "app_server"{ 
+    resource "aws_instance" "deep_server"{ 
         ami = "ami-0aeeebd8d2ab47354"  // platform or OS to use
         instance_type = "t2.micro" // server storage and memory config
 
